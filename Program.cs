df9c51ff9cd77ce734a6f8a2c1ba1f9c28ca8e7c
DaySet909 <78254936+DaySet909@users.noreply.github.com>
2022-05-31 01:23:57 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pictures = 52;
+            int oneRaw = 3;
+            int raws;
+            int overPictures;
+
+            raws = pictures / oneRaw;
+            overPictures = pictures - raws * oneRaw;
+
+            Console.WriteLine("Всего картинок: " + pictures);
+            Console.WriteLine("Полностью заполненых рядов: " + raws);
+            Console.WriteLine("Картинок сверх меры: " + overPictures);
+
+        }
+    }
+}
